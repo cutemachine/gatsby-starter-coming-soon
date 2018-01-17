81ed4aca49956c389fef20adebca5e2d4f1f623a
Jo Meenen <jo@meenen.de>
2018-01-17 08:49:02 +0100

    Add rough draft for the coming soon page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,21 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Awesome Project</h1>
+    <small>Project Name</small>
+    <h1>The Perfect Landing Page For Your Project</h1>
+    <p>The easiest way to build and host a website for your mobile application.</p>
+    <hr />
+    <h2>Coming very soon</h2>
+    <ul>
+      <li>Works perfectly on any screen size</li>
+      <li>Deploy your site with one command</li>
+      <li>Use your own domain</li>
+      <li>No recurring costs</li>
+      <li>Free hosting on Netlify</li>
+      <li>Free certificate from Let's Encrypt</li>
+    </ul>
+    <hr />
+    <p>Email sign-up form to follow</p>
   </div>
 )
 
