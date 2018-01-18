cbcffca18b0c2c8febb94d9526cc09d2401b43bd
Jo Meenen <jo@meenen.de>
2018-01-18 08:58:12 +0100

    Add a central point for the site configuration

---
+++ data/siteConfig.js
@@ -0,0 +1,16 @@
+export default {
+  siteTitle: 'Comin Soon Starter', // Site title, used mainly on the title tag.
+  userLinks: [
+    {
+      label: 'Twitter',
+      url: 'https://twitter.com/YOUR_TWITTER_HANDLE',
+      iconClassName: 'fa fa-twitter'
+    },
+    {
+      label: 'Email',
+      url: 'mailto:YOU@YOUR_DOMAIN.com',
+      iconClassName: 'fa fa-envelope'
+    }
+  ],
+  copyright: 'Copyright © 2017. CuteMachine' // Copyright string
+}
